5397eec477bf886a56fecb5c9d75b707e1628f26
ppsapkota <sapkotapunya@hotmail.com>
2018-04-10 13:55:51 +0300

    Update

--- R/r_ps_displacement_tracking.R
+++ R/r_ps_displacement_tracking.R
@@ -75,6 +75,76 @@ d_departure_B3<-rename(d_departure_B3,"admin1Name_departed_to"="admin1Name_en",
                      "admin3Name_departed_to"="admin3Name_en",
                      "admin4Name_departed_to"="admin4Name_en")
 
+##--GET field index for rearranging--------
+d_main_i <- d_main %>% 
+            select(1:13,admin1Name_en:admin4Name_en,`rep_period/A2`:`rep_period/A2_2`, "_index")
+#d_main_i<-d_main[,c(1:13,admin1Name_en_ind:admin4Name_en_ind,14:22,67)]
+#
+d_main_i<-rename(d_main_i,"_parent_index"="_index")
+
+#new IDP
+d_new_idp_B1<-d_main_i %>% 
+              left_join(d_new_idp_B1,by=c("_parent_index"="_parent_index")) %>% 
+              drop_na_("_index")
+##returnees
+d_returnees_B2 <- d_main_i %>% 
+                  left_join(d_returnees_B2,by=c("_parent_index"="_parent_index")) %>% 
+                  drop_na_("_index")
+##departures
+d_departure_B3<-d_main_i %>% 
+                left_join(d_departure_B3,by=c("_parent_index"="_parent_index")) %>% 
+                drop_na_("_index")
+
+#---------------QUALITY CHECK----------------
+#CHECK 1 - If a community has both monthly data and data reported over a priod
+df_data_period<-data.frame(rep_period_code=c(1,2),rep_period_label=c("monthly", "specified_period"))
+chk_d_new_idp_B1<-d_new_idp_B1
+chk_d_new_idp_B1 <- chk_d_new_idp_B1 %>% 
+                    mutate_at(.funs=funs(as.integer),.vars=vars(`rep_period/A2`)) %>% 
+                    left_join(df_data_period, by= c("rep_period/A2"="rep_period_code"))
+
+
+d_chk_data_period <- chk_d_new_idp_B1 %>%
+                     unite("unite",rep_period_label,`rep_period/A2_2`) %>% 
+                     group_by(`location/adm4` ,unite, `rep_period/A2`,`new_idp/B1/B1_11`,`new_idp/B1/B1_12`,`new_idp/B1/B1_13`,`new_idp/B1/B1_14`) %>%
+                     summarise(nDuplicate = n()) %>% 
+                     spread (unite,nDuplicate)
+ncol_e <-ncol(d_chk_data_period)
+d_chk_data_period$chk_record_if_more_than_1 <- rowSums(d_chk_data_period[7:ncol_e], na.rm=TRUE)
+View(d_chk_data_period)
+
+#CHECK 2 - If a community has more than one record for the same community of origin
+d_chk_data_dupRecords <- chk_d_new_idp_B1 %>%
+                     group_by(`location/adm4` ,`rep_period/A2_2`, `new_idp/B1/B1_11`,`new_idp/B1/B1_12`,`new_idp/B1/B1_13`,`new_idp/B1/B1_14`) %>%
+                     summarise(nDuplicate = n()) %>% 
+                     spread(`rep_period/A2_2`,nDuplicate)
+ncol_e <-ncol(d_chk_data_dupRecords)
+d_chk_data_dupRecords$chk_record_if_more_than_1 <- rowSums(d_chk_data_dupRecords[6:ncol_e], na.rm=TRUE)
+
+View(d_chk_data_dupRecords)
+
+#CHECK 3 - If more than one enumerator entered data or not for the same community
+d_chk_data_dupEnum <- chk_d_new_idp_B1 %>%
+                      group_by(`location/adm4`,`metadata/X2`,`new_idp/B1/B1_11`,`new_idp/B1/B1_12`,`new_idp/B1/B1_13`,`new_idp/B1/B1_14`) %>%
+                      summarise(nDuplicate = n()) %>% 
+                      spread(`metadata/X2`,nDuplicate)
+ncol_e <-ncol(d_chk_data_dupEnum)
+d_chk_data_dupEnum$chk_record_if_more_than_1 <- rowSums(d_chk_data_dupEnum[6:ncol_e], na.rm=TRUE)
+View(d_chk_data_dupEnum)
+
+#CHECK 4 - If more than one record for the same location of origin to the same destination community
+d_chk_nRecords_perDestination <-chk_d_new_idp_B1 %>%
+                                group_by(`location/adm4`) %>% 
+                                summarise(nRecords = n())
+
+d_chk_data_dupOrigin <- chk_d_new_idp_B1 %>%
+                        group_by(`location/adm4`,`new_idp/B1/B1_11`,`new_idp/B1/B1_12`,`new_idp/B1/B1_13`,`new_idp/B1/B1_14`) %>% 
+                        summarise(nDuplicate = n()) %>% 
+                        left_join(d_chk_nRecords_perDestination,by=c("location/adm4"))
+View(d_chk_data_dupOrigin)
+
+
+#--------SAVE file----------
 #---prepare save files---------------
 wb<-openxlsx::createWorkbook()
 addWorksheet(wb,"displacement_tracking")
@@ -83,39 +153,9 @@ addWorksheet(wb,"returnees_B2")
 addWorksheet(wb,"departure_B3")
 #
 writeDataTable(wb,sheet="displacement_tracking",x=d_main,tableName ="tbl_displacement_tracking")
-
-##--GET field index for rearranging--------
-d_main_i <- d_main %>% 
-            select(1:13,admin1Name_en:admin4Name_en,`rep_period/A2`:`rep_period/A2_2`, "_index")
-#d_main_i<-d_main[,c(1:13,admin1Name_en_ind:admin4Name_en_ind,14:22,67)]
-
-#
-d_main_i<-rename(d_main_i,"_parent_index"="_index")
-
-#new IDP
-d_new_idp_B1<-d_main_i %>% 
-              left_join(d_new_idp_B1,by=c("_parent_index"="_parent_index")) %>% 
-              drop_na_("_index")
-#write.xlsx2(d_new_idp_B1,s_filename, sheetName = "new_idp_B1",row.names = FALSE)
 writeDataTable(wb,sheet="new_idp_B1",x=d_new_idp_B1,tableName ="tbl_new_idp_B1")
-
-##returnees
-d_returnees_B2 <- d_main_i %>% 
-                  left_join(d_returnees_B2,by=c("_parent_index"="_parent_index")) %>% 
-                  drop_na_("_index")
-
-#write.xlsx2(d_returnees_B2,s_filename, sheetName = "returnees_B2",row.names = FALSE)
 writeDataTable(wb,sheet="returnees_B2",x=d_returnees_B2,tableName ="tbl_returnees_B2")
-
-##departures
-d_departure_B3<-d_main_i %>% 
-                left_join(d_departure_B3,by=c("_parent_index"="_parent_index")) %>% 
-                drop_na_("_index")
-#write.xlsx2(d_departure_B3,s_filename, sheetName = "departure_B3",row.names = FALSE)
 writeDataTable(wb,sheet="departure_B3",x=d_departure_B3,tableName ="tbl_departure_B3")
-
-
-#--------SAVE file----------
 openxlsx::saveWorkbook(wb,s_filename,overwrite = TRUE)
 
 print("----DONE----")
